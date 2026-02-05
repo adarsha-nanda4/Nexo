3b166ddfa020a2396df653b03cb037d0b22fbb02
Anshuman Rath <94098687+rath-anshuman@users.noreply.github.com>
2026-02-05 21:29:01 +0530

    ..

--- nexo/settings.py
+++ nexo/settings.py
@@ -63,31 +63,31 @@ WSGI_APPLICATION = 'nexo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
-# if os.getenv("DB_ENGINE") == "sqlite":
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.postgresql",
-#             "NAME": os.getenv("NEON_DB_NAME"),
-#             "USER": os.getenv("NEON_DB_USER"),
-#             "PASSWORD": os.getenv("NEON_DB_PASSWORD"),
-#             "HOST": os.getenv("NEON_DB_HOST"),
-#             "PORT": os.getenv("NEON_DB_PORT"),
-#         }
-#     }
+if os.getenv("DB_ENGINE") == "sqlite":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("NEON_DB_NAME"),
+            "USER": os.getenv("NEON_DB_USER"),
+            "PASSWORD": os.getenv("NEON_DB_PASSWORD"),
+            "HOST": os.getenv("NEON_DB_HOST"),
+            "PORT": os.getenv("NEON_DB_PORT"),
+        }
+    }
 
 
 # Password validation
